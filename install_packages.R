ef49339e12529814da19209f228583aec2c7a80e
richardjtelford <richard.telford@bio.uib.no>
2020-03-03 18:01:26 +0100

    RSQLite not RSQlite DBI & osfr automatically installed as dependencies set upgrade = FALSE to avoid annoying upgrade menu

--- install_packages.R
+++ install_packages.R
@@ -4,13 +4,11 @@
 # Install packages from CRAN:
 install.packages("tidyverse")
 install.packages("vegan")
-install.packages("RSQlite")
-install.packages("DBI")
+install.packages("RSQLite")
 install.packages("remotes")
 install.packages("patchwork")
-install.packages("osfr")
 
 # Install libraries from GitHub:
-remotes::install_github("gavinsimpson/ggvegan")
-remotes::install_github("Between-the-Fjords/dataDownloader")
+remotes::install_github("gavinsimpson/ggvegan", upgrade = FALSE)
+remotes::install_github("Between-the-Fjords/dataDownloader", upgrade = FALSE)
 
