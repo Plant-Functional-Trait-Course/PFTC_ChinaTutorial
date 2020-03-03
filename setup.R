31c65a5787ee0045f8de3415d3d735096d5a15bc
richardjtelford <richard.telford@bio.uib.no>
2020-03-03 23:41:08 +0100

    Simplify package installation

--- setup.R
+++ setup.R
@@ -1,19 +1,13 @@
-
 # Run this script first to check required packages installed 
 
 # Check recent version of R installed
-if(getRversion() < "3.6.1") {
+if(getRversion() < "3.6.0") {
   stop("##########\nOld version of R\nPlease install latest version\n##########")
 }
 
 # Check recent version of Rstudio installed
-if(RStudio.Version()$version < "1.0.0"){
+if(RStudio.Version()$version < "1.0.1"){
   stop("##########\nOld version of Rstudio\nPlease install latest version\n##########")
-}
-
-#Check git is installed
-if(Sys.which("git") == ""){
-  warning("##########\ngit not installed\n##########", immediate. = TRUE)
 }
 
 
@@ -25,7 +19,6 @@ CRAN_needed <- c(
   "DBI",
   "remotes",
   "patchwork",
-  "assertthat",
   "rmarkdown")
 
 # check against currently installed packages
@@ -37,17 +30,14 @@ if(length(CRAN_needed2) > 0){
   install.packages(CRAN_needed2)
 }
 
-# check github packages
-github_needed <- read.table(header = TRUE, stringsAsFactors = FALSE, text =
-   "package repo ref
-   ggvegan gavinsimpson master
-   dataDownloader Between-the-Fjords master")
-
+# Install libraries from GitHub:
+remotes::install_github("gavinsimpson/ggvegan", upgrade = FALSE)
+remotes::install_github("Between-the-Fjords/dataDownloader", upgrade = FALSE)
 # check all packages downloaded 
 # if this line doesn't work - assert that didn't install
 
-assertthat::assert_that(all(c(CRAN_needed, github_needed$package) %in% .packages(all.available = TRUE)))
+stopifnot(all(c(CRAN_needed, "ggvegan", "dataDownloader") %in% .packages(all.available = TRUE)))
 
 # clean-up
-rm(CRAN_needed, CRAN_needed2, github_needed, installed_packages)
+rm(CRAN_needed, CRAN_needed2, installed_packages)
 
