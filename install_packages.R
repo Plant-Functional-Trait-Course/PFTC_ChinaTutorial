8a06abf8f6d1141dc202ce79584bce3912b27338
richardjtelford <richard.telford@bio.uib.no>
2020-03-03 17:48:53 +0100

    ggvegan not on cran. Use remotes directly

--- install_packages.R
+++ install_packages.R
@@ -4,15 +4,13 @@
 # Install packages from CRAN:
 install.packages("tidyverse")
 install.packages("vegan")
-install.packages("ggvegan")
 install.packages("RSQlite")
-install.packages("DBI")# also needs RSQLite installed
-install.packages("devtools")
+install.packages("DBI")
+install.packages("remotes")
 install.packages("patchwork")
 install.packages("osfr")
 
 # Install libraries from GitHub:
-library(devtools)
 remotes::install_github("gavinsimpson/ggvegan")
-devtools::install_github("Between-the-Fjords/dataDownloader")
+remotes::install_github("Between-the-Fjords/dataDownloader")
 
